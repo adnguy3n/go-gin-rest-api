3af91d61febc4189948fb714704abdb62615178a
adnguy3n <nguyeant@mail.gvsu.edu>
2023-06-23 00:24:08 -0400

    Added an endpoint that returns a json when hit.

--- main.go
+++ main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func homePage( w http.ResponseWriter, r *http.Request) {
+/*
+ * Item data structure.
+ */
+type Item struct {
+	Name    string `json:"Name"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+/*
+ * Array of Items.
+ */
+type Items []Item
+
+/*
+ * All items endpoint. Returns a json response of all items when hit.
+ */
+func allItems(w http.ResponseWriter, r *http.Request) {
+	items := Items{
+		Item{Name: "Test Item", Desc: "Test Item Description", Content: "Wah!"},
+	}
+
+	fmt.Println("Hit all items endpoint")
+	json.NewEncoder(w).Encode(items)
+}
+
+/*
+ * HomePage endpoint. Prints out a message when hit.
+ */
+func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi, this is Home")
 }
 
+/*
+ * Main function
+ */
 func main() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/items", allItems)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
